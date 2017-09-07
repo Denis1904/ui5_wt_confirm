4bd25f44089c05178c53e3305c193b3e8b2deac8
dyz <denis.yuzhakov@salt-solutions.de>
2017-09-07 09:37:04 +0000

    Login Dialog view and controller added

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -1,12 +1,14 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/m/Dialog",
+	"sap/m/Button"
+], function(Controller, MessageToast, Dialog, Button) {
 	"use strict";
 
 	return Controller.extend("ui5_wt_confirm.controller.Login", {
 
-		onShowHello : function () {
+		onShowHello: function() {
 			// read msg from i18n model
 			var oBundle = this.getView().getModel("i18n").getResourceBundle();
 			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
@@ -14,6 +16,26 @@ sap.ui.define([
 
 			// show message
 			MessageToast.show(sMsg);
+		},
+
+		onLogin: function() {
+			if (!this.pressDialog) {
+				this.pressDialog = new Dialog({
+					title: 'Available Products',
+					//content: 
+					beginButton: new Button({
+						text: 'Close',
+						press: function() {
+							this.pressDialog.close();
+						}.bind(this)
+					})
+				});
+
+				//to get access to the global model
+				this.getView().addDependent(this.pressDialog);
+			}
+
+			this.pressDialog.open();
 		}
 	});
 
