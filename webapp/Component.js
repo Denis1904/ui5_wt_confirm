94cd1401eae3a5ac1091276d0ea7cbc76b67de7c
Yuzhakov Denis <denis.yuzhakov@salt-solutions.de>
2017-09-07 07:02:28 +0000

    - manifest and component added - App.view and Controller are set up

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/Device",
-	"ui5_wt_confirm/model/models"
-], function(UIComponent, Device, models) {
+	"ui5_wt_confirm/model/models",
+	"sap/ui/model/json/JSONModel"
+], function(UIComponent, Device, models, JSONModel) {
 	"use strict";
 
 	return UIComponent.extend("ui5_wt_confirm.Component", {
@@ -17,11 +18,17 @@ sap.ui.define([
 		 * @override
 		 */
 		init: function() {
-			// call the base component's init function
+			// call the init function of the parent
 			UIComponent.prototype.init.apply(this, arguments);
 
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
+			// set data model
+			var oData = {
+				recipient: {
+					name: "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.setModel(oModel);
 		}
 	});
 });
